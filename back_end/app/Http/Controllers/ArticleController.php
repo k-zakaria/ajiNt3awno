2ea9a5833f607d138ed7bbf7c1fc625207fa402e
k-zakaria <kacimizakaria28@gmail.com>
2024-02-17 21:37:00 +0100

    j'ai fais touts les fonctions d'article

--- back_end/app/Http/Controllers/ArticleController.php
+++ back_end/app/Http/Controllers/ArticleController.php
@@ -7,5 +7,97 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    //afficage de touts les articles
+    public function index()
+    {
+        $article = Article::all();
+        return response()->json($article);
+    }
 
+    //affichage un sule article 
+    public function show($id)
+    {
+        $article = Article::find($id);
+
+        if (!$article) {
+            return response()->json(['message' => 'article existe pas.'], 404);
+        }
+
+        return response()->json($article);
+    }
+
+    //ajouter un article
+    public function store(Request $request)
+    {
+        $validation = $request->validate([
+            'title' => 'required|unique',
+            'author' => 'required',
+            'description' => 'required',
+            'content' => 'required',
+            'user_id' => 'required|exists:users,id',
+            'category_id' => 'required|exists:categories,id',
+        ]);
+
+        $article = new Article();
+        $article->title = $request->title;
+        $article->author = $request->author;
+        $article->description = $request->description;
+        $article->content = $request->content;
+        $article->user_id = $request->user_id;
+        $article->category_id = $request->category_id;
+
+
+        $article->save();
+
+        return response()->json('Article ajouté avec succès');
+    }
+
+    //supprimer un article
+    public function destroy($id)
+    {
+        $article = Article::find($id);
+
+        if (!$article) {
+            return response()->json(['message' => 'article existe pas.']);
+        }
+
+        $article->delete();
+
+        return response()->json('article a été supprimée avec succès.');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'description' => 'required',
+            'content' => 'required',
+            'user_id' => 'required|exists:users,id',
+            'category_id' => 'required|exists:categories,id',
+        ]);
+
+        $article = Article::find($id);
+
+        if (!$article) {
+            return response()->json(['message' => 'article existe pas.'], 404);
+        }
+
+        $article->title = $request->title;
+        $article->author = $request->author;
+        $article->description = $request->description;
+        $article->content = $request->content;
+        $article->user_id = $request->user_id;
+        $article->category_id = $request->category_id;
+
+        $article->save();
+
+        return response()->json(['message' => 'article a été mis à jour avec succès.', 'article' => $article]);
+    }
+
+    //search article par "title"
+
+    public function search($title){
+        return Article::where('title', 'like', '%'.$title.'%')->get();
+    }
 }
--- back_end/routes/api.php
+++ back_end/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,19 +18,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/article',[ArticleController::class,'index']);
-Route::post('/article',[ ArticleController::class, 'store']);
-Route::post('/article/{id}',[ ArticleController::class, 'destroy']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    
+    Route::resource('/article', ArticleController::class);
+    Route::post('/logout',[AuthController::class, 'logout']);
+});
+
+//CRUD article
+Route::get('/article/search/{title}',[ArticleController::class, 'search']);
+
 
 //CRUD category
 Route::resource('/categorie', CategoryController::class);
-
-// Route::put('updateProduct',[ProductsController::class,'updateProduct']);
-// Route::post('addProduct',[ProductsController::class,'addProduct']);
-// Route::delete('deleteProduct',[ProductsController::class,'deleteProduct']);
-// Route::get('getProductById/{id}',[ProductsController::class,'getProductById']);
-// Route::get('getProductInProduct/{id}',[ProductsController::class,'getProductInProduct']);
